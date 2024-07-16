44c951f56dc88bdd0f719c767b231a2ebfc599ec
Jhowmsm <jhownsman@gmail.com>
2024-07-16 20:40:33 +0000

    Last commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ["El perro", "Mi abuela", "El cartero", "El cumpleañero"];
+let action = ["se comió", "mojó", "aplastó", "dañó"];
+let what = ["mi trabajo", "mi teléfono", "el carro"];
+let when = [
+  "antes de clase",
+  "cuando dormía",
+  "mientras hacía ejercicio",
+  "durante la comida",
+  "mientras rezaba"
+];
+// Función para generar una excusa aleatoria
+function generarExcusa() {
+  // Función para obtener un elemento aleatorio de un array
+  function obtenerElementoAleatorio(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
+  // Generar una excusa
+  let excusa = `${obtenerElementoAleatorio(who)} ${obtenerElementoAleatorio(
+    action
+  )} ${obtenerElementoAleatorio(what)} ${obtenerElementoAleatorio(when)}.`;
+
+  return excusa;
+}
+let excusaGenerada = generarExcusa();
+
+document.getElementById("excusa").innerHTML = excusaGenerada;
